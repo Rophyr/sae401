a7757aca8f2c8138e0bfb4865686610fc7ea9eec
AmandineVrn <amandinevrne@gmail.com>
2024-03-15 10:17:41 +0100

    started front on classement.vue

--- src/views/Classement.vue
+++ src/views/Classement.vue
@@ -7,46 +7,59 @@
 
         <div class="container container__classement">
 
-            <div class="classement__left">
+            <div class="container__left">
             <router-link to="/"><img src="/public/images/logo_glasses.svg" alt=""></router-link>
+            
+            
+           
+
+            <button @click="goToMenu" class="btn btn--green btn--round"><img src="/images/back_door.svg" aria-label="retourner au menu"></button>
+      
+
+        </div>
+        <div class="container__middle">
+
+            <div class="title">
+                <h1>CLASSSEMENT</h1>
+                <div class="green-bar"></div>
+            </div>
+
+            <div class="tableau">
+                
+                <table>
+                <tr>
+                    <th>Top</th>
+                    <th>Temps</th>
+                    <th>Pseudo</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>00:00:00</td>
+                    <td>Enzo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>00:00:00</td>
+                    <td>Julien</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>00:00:00</td>
+                    <td>Amandine</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>00:00:00</td>
+                    <td>Alyson</td>
+                </tr>
+            </table>
+            </div>
             <div class="niveau">
                 <button class="btn btn--facile">Facile</button>
                 <button class="btn btn--moyen">Intermédiaire</button>
                 <button class="btn btn--difficile">Difficile</button>
                 
                 
-            </div>
-            
-           
-
-            
-
-        </div>
-        <div class="classement__right">
-            <div class="tableau">
-                <h2 class="titre">Classement</h2>
-                <table>
-                <tr>
-                    <th>Top</th>
-                    <th>Temps</th>
-                    <th>Date</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>00:00:00</td>
-                    <td>01/01/2021</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>00:00:00</td>
-                    <td>01/01/2021</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>00:00:00</td>
-                    <td>01/01/2021</td>
-                </tr>
-            </table>
             </div>
         </div>
     </div>
