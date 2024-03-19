d025e36827a9ccd26957927f7adec1d5e8f80107
Julien Gioffredi <106537110+TheGoldDev@users.noreply.github.com>
2024-03-19 11:14:18 +0100

    Optimisation Facile.vue - Game.vue

---
+++ src/components/Game.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="grid game"> <!-- JEU -->
+    <div class="row" v-for="(row, rowIndex) in grid" :key="rowIndex">
+      <div class="container" v-for="(card, cardIndex) in row" :key="cardIndex" @click="flipCard(rowIndex, cardIndex)">
+        <div class="card" :class="{ active: card.isFlipped }">
+          <div class="front">
+            <img draggable="false" :src="getCardImagePath(rowIndex, cardIndex)" alt="carte retournée">
+          </div>
+          <div class="back">
+            <img draggable="false" :src="card.backImagePath" :alt="getCardAlt(card.backImagePath)">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import objectsData from '../../public/data/objectDescription.json';
+
+const objectDescription = ref('');
+let word = "RATEAU"; //debug
+
+const rows = 3;
+const columns = 4;
+const totalCards = rows * columns;
+const numEachType = totalCards / 6; // Il y a 6 types de cartes
+
+let winCount = 0;
+
+const grid = ref(Array.from({ length: rows }, () => Array.from({ length: columns }, () => ({
+  isFlipped: false,
+  backImagePath: ''
+}))));
+
+let flippedCard = null;
+let timerComponent = null;
+
+function flipCard(rowIndex, cardIndex) {
+  const card = grid.value[rowIndex][cardIndex];
+
+  if (!card.isFlipped) {
+    card.isFlipped = true;
+
+    if (!flippedCard) {
+      flippedCard = { rowIndex, cardIndex, backImagePath: card.backImagePath };
+    } else {
+      const { rowIndex: prevRowIndex, cardIndex: prevCardIndex, backImagePath: prevBackImagePath } = flippedCard;
+
+      if (rowIndex === prevRowIndex && cardIndex === prevCardIndex) {
+        return;
+      }
+
+      if (card.backImagePath === prevBackImagePath) {
+        grid.value[prevRowIndex][prevCardIndex].isFlipped = true;
+        card.isFlipped = true;
+        winCount++;
+
+        const objectName = card.backImagePath.split("/").pop().split(".")[0];
+        objectDescription.value = objectsData[objectName];
+        if (winCount === 6) {
+          document.getElementById('victory').style.display = "flex"
+          timerComponent.stopTimer();
+          console.log('Recommencer')
+        }
+      } else {
+        setTimeout(() => {
+          grid.value[prevRowIndex][prevCardIndex].isFlipped = false;
+          card.isFlipped = false;
+        }, 500);
+      }
+
+      flippedCard = null;
+    }
+  }
+}
+
+function getCardImagePath(rowIndex, cardIndex) {
+  return "/images/facile/back-facile.svg";
+}
+
+function getBackImagePath(rowIndex, cardIndex) {
+  const types = [
+    { image: "/images/facile/vert-rateau.svg", count: 0 },
+    { image: "/images/facile/vert-arrosoir.svg", count: 0 },
+    { image: "/images/facile/vert-pelle.svg", count: 0 },
+    { image: "/images/facile/vert-solaire.svg", count: 0 },
+    { image: "/images/facile/trash.png", count: 0 },
+    { image: "/images/facile/velo.png", count: 0 }
+  ];
+
+  for (let i = 0; i < grid.value.length; i++) {
+    for (let j = 0; j < grid.value[i].length; j++) {
+      const backImagePath = grid.value[i][j].backImagePath;
+      const index = types.findIndex(type => type.image === backImagePath);
+      if (index !== -1) {
+        types[index].count++;
+      }
+    }
+  }
+
+  let selectedType = null;
+  do {
+    const randomIndex = Math.floor(Math.random() * types.length);
+    if (types[randomIndex].count < numEachType) {
+      selectedType = types[randomIndex];
+      selectedType.count++;
+    }
+  } while (!selectedType);
+
+  switch (selectedType.image) {
+    case "/images/facile/vert-rateau.svg":
+      numRateau++;
+      break;
+    case "/images/facile/vert-arrosoir.svg":
+      numArrosoir++;
+      break;
+    case "/images/facile/vert-solaire.svg":
+      numSolaire++;
+      break;
+    case "/images/facile/vert-pelle.svg":
+      numPelle++;
+      break;
+    case "/images/facile/trash.png":
+      numTrash++;
+      break;
+    case "/images/facile/velo.png":
+      numVelo++;
+      break;
+  }
+
+  return selectedType.image;
+}
+
+let numRateau = 0;
+let numArrosoir = 0;
+let numSolaire = 0;
+let numPelle = 0;
+let numTrash = 0;
+let numVelo = 0;
+
+for (let i = 0; i < grid.value.length; i++) {
+  for (let j = 0; j < grid.value[i].length; j++) {
+    grid.value[i][j].backImagePath = getBackImagePath(i, j);
+  }
+}
+
+function getCardAlt(imagePath) {
+  // Déterminez l'alt en fonction du chemin de l'image
+  // Vous pouvez le personnaliser selon vos besoins
+  return "Carte " + imagePath.split('/').pop().split('.')[0];
+}
+</script>
--- src/views/Facile.vue
+++ src/views/Facile.vue
@@ -1,50 +1,25 @@
 <template>
   <div id="app" class="jeux jeux--facile">
     <div class="container container__facile">
-      
-
       <div class="container__left">
         <router-link to="/"><img src="/public/images/logo_glasses.svg" alt=""></router-link>
-        
         <div  class="object-description explain">
-            
           <div class="word">
             <span v-for="(letter, i) in word" :key="i" :class="'lettre-' + i">{{ letter }}</span>
           </div>
-
-
               <p>{{ objectDescription }}</p>
-            
           </div>
-
         <button @click="goToMenu" class="btn btn--green btn--round btn--round--col"><img src="/images/back_door.svg" aria-label="retourner au menu"></button>
-      
       </div> <!-- Fin container left -->
-
       <div class="container__middle">
-
         <div class="grid game"> <!-- JEU -->
-          <div class="row" v-for="(row, rowIndex) in grid" :key="rowIndex">
-            <div class="container" v-for="(card, cardIndex) in row" :key="cardIndex" @click="flipCard(rowIndex, cardIndex)">
-              <div class="card" :class="{ active: card.isFlipped }">
-                <div class="front">
-                  <img draggable="false" :src="getCardImagePath(rowIndex, cardIndex)" alt="carte retourné">
-                </div>
-                <div class="back">
-                  <img draggable="false" :src="card.backImagePath" alt="Back face" :alt="carte">
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> <!-- Fin JEU -->
+          <Game />
       </div>
-
       <div class="container__right">
-        
         <Timer ref="timerComponent"/>
-        <!-- <p id="timer"></p> -->
       </div>
     </div>
+  </div>
   </div>
   <div id="victory" style="display: none">
     <p id="vic-text">Bravo !</p>
@@ -57,157 +32,22 @@
 </template>
 
 
-<script setup>
-//import Timer from '../components/Timer.vue'
-import '../assets/styles.css';  //link css / scss
-import { ref } from 'vue';
-import objectsData from '../../public/data/objectDescription.json';
-const objectDescription = ref('');
-let word = "RATEAU"; //debug
 
-const rows = 3;
-const columns = 4;
-const totalCards = rows * columns;
-const numEachType = totalCards / 6; // Il y a 6 types de cartes
-
-let winCount = 0;
-
-const grid = ref(Array.from({ length: rows }, () => Array.from({ length: columns }, () => ({ isFlipped: false, backImagePath: '' }))));
-
-let flippedCard = null;
-let timerComponent = null;
-
-// eslint-disable-next-line no-unused-vars
-// let timerComponent = null;
-
-function flipCard(rowIndex, cardIndex) {
-  const card = grid.value[rowIndex][cardIndex];
-
-  if (!card.isFlipped) {
-    card.isFlipped = true;
-
-    if (!flippedCard) {
-      flippedCard = { rowIndex, cardIndex, backImagePath: card.backImagePath };
-    } else {
-      const { rowIndex: prevRowIndex, cardIndex: prevCardIndex, backImagePath: prevBackImagePath } = flippedCard;
-
-      if (rowIndex === prevRowIndex && cardIndex === prevCardIndex) {
-        return;
-      }
-
-      if (card.backImagePath === prevBackImagePath) {
-        grid.value[prevRowIndex][prevCardIndex].isFlipped = true;
-        card.isFlipped = true;
-        winCount++;
-
-        const objectName = card.backImagePath.split("/").pop().split(".")[0];
-        objectDescription.value = objectsData[objectName];
-        if(winCount === 6){
-          document.getElementById('victory').style.display = "flex"
-          timerComponent.stopTimer();
-          console.log('Recommencer')
-        }
-      } else {
-        // Retourner les cartes après une pause de 0.5 seconde
-        setTimeout(() => {
-          grid.value[prevRowIndex][prevCardIndex].isFlipped = false;
-          card.isFlipped = false;
-        }, 500);
-      }
-
-      flippedCard = null;
-    }
-  }
-}
-// eslint-disable-next-line no-unused-vars
-function getCardImagePath(rowIndex, cardIndex) {
-  return "/images/facile/back-facile.svg";
-}
-
-// eslint-disable-next-line no-unused-vars
-function getBackImagePath(rowIndex, cardIndex) {
-  const types = [
-    { image: "/images/facile/vert-rateau.svg", count: 0 },
-    { image: "/images/facile/vert-arrosoir.svg", count: 0 },
-    { image: "/images/facile/vert-pelle.svg", count: 0 },
-    { image: "/images/facile/vert-solaire.svg", count: 0 },
-    { image: "/images/facile/trash.png", count: 0 },
-    { image: "/images/facile/velo.png", count: 0 }
-  ];
-
-  for (let i = 0; i < grid.value.length; i++) {
-    for (let j = 0; j < grid.value[i].length; j++) {
-      const backImagePath = grid.value[i][j].backImagePath;
-      const index = types.findIndex(type => type.image === backImagePath);
-      if (index !== -1) {
-        types[index].count++;
-      }
-    }
-  }
-
-  let selectedType = null;
-  do {
-    const randomIndex = Math.floor(Math.random() * types.length);
-    if (types[randomIndex].count < numEachType) {
-      selectedType = types[randomIndex];
-      selectedType.count++;
-    }
-  } while (!selectedType);
-
-  switch (selectedType.image) {
-    case "/vert-rateau.svg":
-      numRateau++;
-      break;
-    case "/vert-arrosoir.svg":
-      numArrosoir++;
-      break;
-    case "/vert-solaire.svg":
-      numSolaire++;
-      break;
-    case "vert-pelle.svg":
-      numPelle++;
-      break;
-    case "/trash.png":
-      numTrash++;
-      break;
-    case "/velo.png":
-      numVelo++;
-      break;
-  }
-
-  return selectedType.image;
-}
-// eslint-disable-next-line no-unused-vars
-let numRateau = 0;
-// eslint-disable-next-line no-unused-vars
-let numArrosoir = 0;
-// eslint-disable-next-line no-unused-vars
-let numSolaire = 0;
-// eslint-disable-next-line no-unused-vars
-let numPelle = 0;
-// eslint-disable-next-line no-unused-vars
-let numTrash = 0;
-// eslint-disable-next-line no-unused-vars
-let numVelo = 0;
-
-for (let i = 0; i < grid.value.length; i++) {
-  for (let j = 0; j < grid.value[i].length; j++) {
-    grid.value[i][j].backImagePath = getBackImagePath(i, j);
-  }
-}
-
-</script>
 <script>
-
-  export default {
-    methods: {
-      goToMenu() {
-        this.$router.push('/homepage');
-      }
+import Timer from '../components/Timer.vue'
+import Game from '../components/Game.vue';
+export default {
+  methods: {
+    goToMenu() {
+      this.$router.push('/homepage');
     }
-  }
+  },
+  components: {
+    Game,
+    Timer
+  },
+}
 </script>
-
 <style scoped>
 @import url('https://fonts.cdnfonts.com/css/simply-rounded');
 </style>
