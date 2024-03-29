0a1501df8d7f69e74decdb3757db10da76a4018e
AmandineVrn <amandinevrne@gmail.com>
2024-03-29 14:41:48 +0100

    trad mentions légales

--- src/views/MentionsLegales.vue
+++ src/views/MentionsLegales.vue
@@ -8,29 +8,28 @@
 
           <div class="container__middle">
             <div class="text">
-              <h1 class="text-center">Mentions Légales</h1>
+              <h1 class="text-center">{{$t('mentionsLegales.title')}}</h1>
 
-              <p class="titre">1. Identité du propriétaire</p>
-              <p>Le jeu FROGGY est édité par : Alyson Bertille, Clément De Roberti, Julien Gioffredi, Enzo Latu et Amandine Verneau.</p>
+              <p class="titre">{{$t('mentionsLegales.t1')}}</p>
+              <p>{{$t('mentionsLegales.p1')}}</p>
           
-              <p class="titre">2. Mentions relatives aux droits d'auteur</p>
-              <p>L'ensemble des contenus du jeu FROGGY, tels que les textes, les images, les vidéos, et les sons, sont protégés par le droit d'auteur.
-              Toute reproduction ou utilisation de ces contenus sans l'autorisation expresse de l'éditeur est interdite.</p>
+              <p class="titre">{{$t('mentionsLegales.t2')}}</p>
+              <p>{{$t('mentionsLegales.p2')}}</p>
           
-              <p class="titre">3. Mentions relatives aux liens hypertextes</p>
-              <p>Le jeu FROGGY peut contenir des liens hypertextes vers d'autres sites web. L'éditeur décline toute responsabilité quant au contenu de ces sites web.</p>
+              <p class="titre">{{$t('mentionsLegales.t3')}}</p>
+              <p>{{$t('mentionsLegales.p3')}}</p>
           
-              <p class="titre">4. Mise à jour des mentions légales</p>
-              <p>Les mentions légales du jeu FROGGY sont susceptibles d'être modifiées à tout moment. La dernière version des mentions légales est disponible sur cette même page.</p>
+              <p class="titre">{{$t('mentionsLegales.t4')}}</p>
+              <p>{{$t('mentionsLegales.p4')}}</p>
           
-              <p class="titre">5. Publication des mentions légales</p>
-              <p>Les mentions légales du jeu FROGGY sont publiées de manière visible et accessible sur l'application. Elles sont accessibles à tout utilisateur, sans avoir à créer un compte ou à fournir des informations personnelles.</p>
+              <p class="titre">{{$t('mentionsLegales.t5')}}</p>
+              <p>{{$t('mentionsLegales.p5')}}</p>
           
-              <p class="titre">6. Sanctions en cas de non-respect des mentions légales</p>
-              <p>Le non-respect des mentions légales peut entraîner des sanctions administratives, telles que des amendes. Dans certains cas, il peut également entraîner des sanctions pénales.</p>
+              <p class="titre">{{$t('mentionsLegales.t6')}}</p>
+              <p>{{$t('mentionsLegales.p6')}}</p>
           
-              <p class="titre">7. Contact</p>
-              <p>Pour toute question relative aux mentions légales du jeu FROGGY, vous pouvez contacter l'éditeur à l'adresse électronique suivante : <a href="mailto:john.doe@mmi-troyes.fr">john.doe@mmi-troyes.fr</a>.</p>
+              <p class="titre">{{$t('mentionsLegales.t7')}}</p>
+              <p>{{$t('mentionsLegales.p7')}} <a href="mailto:john.doe@mmi-troyes.fr">john.doe@mmi-troyes.fr</a>.</p>
                 
             </div>
           </div>
